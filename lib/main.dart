506314e542e961d86a53f4fee2ea18f0a91f237a
Cawies <calle.essman@gmail.com>
2020-09-30 18:30:38 +0300

    testing with a bar and a little text

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
+// Import material themes to simplify design
+import 'package:flutter/material.dart';
 
+// Flutter uses a main function to execute parts of the code.
+void main() {
+  runApp(StepOne());
+}
+
+// Flutter uses extensions of base classes to simplify rendering things on the screen.
+class StepOne extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('A small first step.'),
+        ),
+        body: Text('Put some content here.'),
+      ),
+    );
+  }
+}
